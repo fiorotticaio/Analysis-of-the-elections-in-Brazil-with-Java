8e38dd0cc756c0d15f75190d8b13b863254dc466
fiorotticaio <caiofiorotti@gmail.com>
2022-10-17 22:43:16 -0300

    :poop: adding args requirement warning

--- deputados/src/App.java
+++ deputados/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        if (args.length < 4) {
+            System.out.println("Use: java -jar deputados.jar <opção_de_cargo> <caminho_arquivo_candidatos> <caminho_arquivo_votacao> <data>");
+            System.exit(1);
+        }
+
+        
     }
 }
