2b3fc4ff47e5ba941f7c6aa865877ef7acb21ca9
fiorotticaio <caiofiorotti@gmail.com>
2022-10-30 15:34:22 -0300

    :white_check_mark: reading the name of all candidates

--- deputados/src/App.java
+++ deputados/src/App.java
@@ -2,41 +2,70 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import eleicao.Candidato;
 
 public class App {
+
+    public static void imprimeCandidatos(List<Candidato> candidatos) {
+        int i = 1;
+        for (Candidato cand : candidatos) {
+            System.out.println(i + " - " + cand.getNome());
+            i++;
+        }
+    }
+
+
     public static void main(String[] args) throws Exception {
         // if (args.length < 4) {
         //     System.out.println("Use: java -jar deputados.jar <opção_de_cargo> <caminho_arquivo_candidatos> <caminho_arquivo_votacao> <data>");
         //     System.exit(1);
         // }
 
-        String arquivoCSV = "./teste.csv";
+        String arquivoCSV = "./consulta_cand_2022_ES.csv";
         BufferedReader br = null;
         String linha = "";
         String csvDivisor = ";";
+
+        List<Candidato> candidatos = new LinkedList<>();
+
+        String colunaDeInteresse = "NM_CANDIDATO";
+
+        int i = 0, j = 0;
+        int index = 0;
 
         try {
             br = new BufferedReader(new FileReader(arquivoCSV));
             while ((linha = br.readLine()) != null) {
 
-                String[] pessoa = linha.split(csvDivisor);
+                String[] infoCandidato = linha.split(csvDivisor);
 
-                /* protótipo de algorítimo pra tirar as aspas */
-                StringBuilder sb1 = new StringBuilder();
-                char[] tab = pessoa[0].toCharArray();
-                for (char current : tab) {
-                    if (current != '"') sb1.append(current);
+                /* algorítimo pra tirar as aspas */
+                for (j = 0; j < infoCandidato.length; j++) {
+                    StringBuilder sb = new StringBuilder();
+                    char[] tab = infoCandidato[j].toCharArray();
+                    for (char current : tab) {
+                        if (current != '"') sb.append(current);
+                    }
+                    String novaString = sb.toString();
+                    infoCandidato[j] = novaString;
                 }
-                sb1.toString();
 
-                StringBuilder sb2 = new StringBuilder();
-                char[] tab2 = pessoa[0].toCharArray();
-                for (char current : tab2) {
-                    if (current != '"') sb2.append(current);
+                /* descobrir o index da coluna "NM_CANDDIDATO" */
+                if (i == 0) {
+                    while (!infoCandidato[i].equals(colunaDeInteresse)) {
+                        i++;
+                    }
+                    index = i;
+                    continue;
                 }
-                sb2.toString();
 
-                System.out.println("[nome= " + sb1 + " , cidade=" + sb2 + "]");
+                Candidato novoCandidato = new Candidato(infoCandidato[index]);
+                candidatos.add(novoCandidato);
+
+                i++;
             }
 
         } catch (FileNotFoundException e) {
@@ -52,5 +81,7 @@ public class App {
                 }
             }
         }
+
+        imprimeCandidatos(candidatos);
     }
 }
